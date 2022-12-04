f44d9d40b8b07a7110d2ee53edf2a709a5d87c8f
danperes97 <danilo.peres@hotmail.com>
2022-12-04 17:09:04 -0300

    Fix layout css

--- src/features/navbar/components/NavBarItem.js
+++ src/features/navbar/components/NavBarItem.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import styles from './NavBarItem.module.css'
+import React from "react";
+import styles from "./NavBarItem.module.css";
 
-const NavBarItem = ({children, phrase}) => {
+const NavBarItem = ({ children, phrase }) => {
   return (
     <div className={styles.navbarItem}>
       {children}
-      <span>{phrase}</span>
+      <span className={styles.phrase}>{phrase}</span>
     </div>
-  )
-}
+  );
+};
 
-export default NavBarItem
+export default NavBarItem;
